c17fac94510eafc17f2da92ac5d1eddbad0ad726
po-the-panda-12 <shambhavig.2019@scis.smu.edu.sg>
2024-01-10 19:20:52 +0800

    content updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 
 const articles = [
-  { id: 'mutualism', title: 'Mutualism among Animals', tags: ['Bird', 'Crododile', 'Marsupial'], place :'Asia', date: "JAN 03, 2024",
+  { id: 'mutualism', title: 'Vested Interests', tags: ['Bird', 'Crododile', 'Marsupial'], place :'Asia', date: "JAN 03, 2024",
   content: ["Once upon a time, deep within the rainforests of Papua New Guinea, there was a tree-kangaroo named Hopper. He was a curious lad who had just outgrown his mama’s pouch and was ready to explore the world. ",
       "Hopper was raised in a loving environment, and in return only had love to give. He was kind of heart and never judged a book by its cover. During his evening climbs, he chanced upon Whistler, the plover bird. Many animals didn’t like Whistler but that didn’t stop Hopper from being kind. Hopper had fun listening to Whistler’s tales of his travels across the rainforest, from the highlands to the marshlands. Soon they started roaming the forest together and grew closer. Hopper’s friends warned him of their friendship due to Whistler’s clandestine allegiance to Snapper the crocodile but Hopper believed that his friendship was pure.",
       '1',
@@ -14,16 +14,18 @@ const articles = [
   audio: 'https://firebasestorage.googleapis.com/v0/b/storytime-1.appspot.com/o/audio%2Fsample.mp3?alt=media&token=ff319216-13e1-4869-a667-8cb91bdbad55'
   },
 
-  { id: 'article2', title: 'Article 2', tags: ['Animal'], place :'Africa', date: "JAN 05, 2024",
-  content: ['Lorem ipsum and all',
-      'Nullam maximus venenatis mauris id lobortis.',
-      '1',
-      'Morbi a porta diam. Interdum et malesuada fames ac ante ipsum primis in faucibus.',
-      '2',
-      'Curabitur consectetur erat eu massa cursus, et viverra lectus hendrerit.'
+  { id: 'friendship', title: 'Forest Friendship', tags: ['Capybara', 'Anteater', 'Monkey'], place :'Africa', date: "JAN 05, 2024",
+  content: [`In a lush and vibrant rainforest, there lived a friendly and wise capybara named Cappy. Cappy was not your average capybara; he possessed a special ability to understand and communicate with all the animals in the forest. This unique gift made him a cherished friend among the various creatures that called the rainforest their home.`,
+      `Cappy's days were filled with laughter and camaraderie as he hopped from one group of animals to another, sharing stories and offering advice. His warm and inviting nature drew others toward him, creating a sense of unity in the forest.`,
+      `One day, a timid and lonely anteater named Andy approached Cappy. With a soft snuffling sound, Andy explained how he felt isolated and longed for companionship. Cappy, recognizing the importance of friendship, decided to help Andy and introduced him to a diverse group of animals.`,
+      `As time passed, Freddy blossomed into a confident and joyful tree frog, surrounded by friends of all shapes and sizes. The once quiet corners of the rainforest echoed with laughter, songs, and the rustling of leaves as the newfound friends shared their unique talents and stories.`,
+      `As time passed, Andy blossomed into a confident and joyful anteater, surrounded by friends of all shapes and sizes. The once quiet corners of the rainforest echoed with laughter, songs, and the rustling of leaves as the newfound friends shared their unique talents and stories.`,
+      `One day, a storm struck the rainforest, causing panic among the animals. Cappy, realizing the importance of unity during difficult times, gathered everyone together. Each animal brought their strengths to the table – the monkeys swung from branch to branch to secure loose leaves, the birds soared through the sky to scout for danger, and the ants worked tirelessly to build shelter.`,
+      `In the face of adversity, the bond between the animals grew stronger. The storm passed, leaving the rainforest battered but resilient. The animals, knowing they could rely on one another, celebrated their success and appreciated the vital role friendship played in overcoming challenges.`,
+      `Cappy, the wise capybara, had not only created a close-knit community but also taught the importance of having friends. His legacy lived on, and the rainforest became a beacon of friendship, where the strength of the individual was surpassed by the power of unity and camaraderie. And so, in the heart of the lush rainforest, the animals continued to thrive, forever grateful for the gift of friendship that Cappy had bestowed upon them.`
   ],
-  images: ['https://firebasestorage.googleapis.com/v0/b/storytime-1.appspot.com/o/images%2Fplover-bird%2Bcroc.png?alt=media&token=c579cc61-9703-4aaf-8387-e438e9716e94', 'https://firebasestorage.googleapis.com/v0/b/storytime-1.appspot.com/o/images%2Fplover-bird%2Bcroc.png?alt=media&token=c579cc61-9703-4aaf-8387-e438e9716e94'],
-  audio: 'https://firebasestorage.googleapis.com/v0/b/storytime-1.appspot.com/o/audio%2Fsample.mp3?alt=media&token=ff319216-13e1-4869-a667-8cb91bdbad55'
+  images: ['https://firebasestorage.googleapis.com/v0/b/storytime-1.appspot.com/o/images%2Fcapybara%2Banteater.png?alt=media&token=023a8add-e6e9-4612-b318-a5ed0bc3be19', 'https://firebasestorage.googleapis.com/v0/b/storytime-1.appspot.com/o/images%2Fcapybara%2Bmonkey.png?alt=media&token=96abd956-cd11-41df-ab2e-6b4b10bbb510'],
+  audio: 'https://firebasestorage.googleapis.com/v0/b/storytime-1.appspot.com/o/audio%2Fcapybara_song.mp3?alt=media&token=c8fe39db-ddef-41e6-83db-b4bc425c2f7d'
   },
 
 ];
@@ -36,15 +38,15 @@ const pointsData = [
             lng: 147.3272, // Latitude and Longitude for Papua New Guinea
             size: 10,
             label: 'Papua New Guinea',
-            info: 'This is Papua New Guinea. Additional information can go here.',
+            info: 'Papua New Guinea has a tale on Vested Interests',
         },
         {
-            id: 'article1',
-            lat: 40.7128,
-            lng: -74.006, // Latitude and Longitude for New York
+            id: 'friendship',
+            lat: -10.9518,
+            lng: -65.3181, // Latitude and Longitude for Bolivia
             size: 15,
-            label: 'New York',
-            info: 'This is New York City. Additional information can go here.',
+            label: 'Bolivia',
+            info: 'Bolivia has a tale on Friendship.',
         },
         {
             id: 'article2',
